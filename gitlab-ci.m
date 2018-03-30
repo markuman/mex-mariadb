a0c80ec0a8f3cb00f6a54ee9866dca2c43569f78
Markus Bergholz <markuman@gmail.com>
2018-03-30 09:50:55 +0200

    update tests

--- gitlab-ci.m
+++ gitlab-ci.m
@@ -4,10 +4,20 @@ function last_words()
     disp ("all test passed")
 end
 
-retval = mariadb('mariadb', 3306, 'root', 'password', 'octave', 'select version() as version')
+%% test raw mariadb mex file
 
+retval = mariadb_('mariadb', 3306, 'root', 'password', 'octave', 'select version() as version')
 assert(2 == length(retval))
 assert(1 == strcmp(retval{1}, 'version'))
 assert(1 == strfind (retval{2}, '10.2.13-MariaDB') || strfind (retval{2}, '5.7.21'))
+
+%% classdef tests
+
+sql = mariadb('hostname', 'mariadb', 'password', 'password');
+retval = sql.query('select version() as version');
+assert(2 == length(retval))
+assert(1 == strcmp(retval{1}, 'version'))
+assert(1 == strfind (retval{2}, '10.2.13-MariaDB') || strfind (retval{2}, '5.7.21'))
+
 
 atexit ("last_words");
