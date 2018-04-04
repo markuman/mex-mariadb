3d1a3f2294d0e2abd223bb274c947580bf9debda
markuman <markuman@gmail.com>
2018-04-04 15:22:19 +0200

    rework gitlabci tests for input argument change and test create and inserting commands

--- gitlabci.m
+++ gitlabci.m
@@ -1,9 +1,9 @@
 %% test raw mariadb mex file
 
 testMex = @(x) strcat('MEX FILE: ', x);
-testClasdef = @(x) strcat('CLASSDEF: ', x);
+testClassdef = @(x) strcat('CLASSDEF: ', x);
 
-retval = mariadb_('mariadb', 3306, 'root', 'password', 'octave', 'select version() as version');
+retval = mariadb_('mariadb', 3306, 'root', 'password', 'select version() as version');
 
 assert(2 == length(retval), 
   testMex('data length must be 2'))
@@ -20,16 +20,37 @@ sql = mariadb('hostname', 'mariadb', 'password', 'password');
 retval = sql.query('select version() as version');
 
 assert(2 == length(retval),
-  testClasdef('data length mus be 2'))
+  testClassdef('data length must be 2'))
 
 assert(1 == strcmp(retval{1}, 'version'),
-  testClasdef('first cell value must be string "version"'))
+  testClassdef('first cell value must be string "version"'))
 
 assert(1 == strfind (retval{2}, '10.2.13-MariaDB') || strfind (retval{2}, '5.7.21'),
-  testClasdef('check for mariadb/mysql version'))
+  testClassdef('check for mariadb/mysql version'))
 
+% test change output format
 sql.output = 'mat';
 retval = sql.query('select version() as version');
 
 assert(1 == ismatrix(retval),
-  testClasdef('return value must be a Matrix'))
+  testClassdef('return value must be a Matrix'))
+
+assert(0 == numel(sql.query('create database octave')),
+  testClassdef('create database octave'))
+
+% test change database
+sql.database = 'octave';
+assert(0 == numel(sql.query('create table a (idx int)')),
+  testClassdef('create table a'))
+
+% test connection with given database
+sql = mariadb('hostname', 'mariadb', 'password', 'password', 'database', 'octave');
+sql.output = 'mat';
+assert(0 == numel(sql.query('insert into a (idx) values (1),(2),(3),(4)')),
+  testClassdef('insert data into table a'))
+
+retval = sql.query('select * from a');
+assert(10 == sum(retval(~isnan(retval))),
+  testClassdef('sum all numbers of table a'))
+assert(isnan(retval(1)),
+  testClassdef('first element is not a number'))
